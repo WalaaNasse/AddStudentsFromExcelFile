bf1e8853e31ad69bdf58a7dfa6733bcdb0128779
WalaaNasse <walaanasser26W@gmail.com>
2023-11-09 13:23:05 +0200

    4commit

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ var requestOptions = {
   redirect: 'follow'
 };
 
-fetch("http://localhost:3000/addStudents", requestOptions)
+fetch("https://excel-api-xd9r.onrender.com/addStudents", requestOptions)
   .then(response => response.text())
   .then(result => console.log(result))
   .catch(error => console.log('error', error));
